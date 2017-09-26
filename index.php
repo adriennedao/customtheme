4f0a99bb5aebd3ddb958cf1f040d0ae7ec7b08ce
AdrienneD <adriennedao@gmail.com>
2017-09-26 12:23:59 -0400

    added files

--- index.php
+++ index.php
@@ -1,33 +1,17 @@
 <?php
 /**
- * The template for displaying the index page
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
  *
- *
- * @package CustomTheme
- * @since Custom Theme 1.0
+ * @package WordPress
  */
-?>
-<?php get_header(); ?>
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-8">
-        <?php if ( have_posts() ) : ?>
-        
-            <?php while ( have_posts() ) : the_post(); ?>
-                
-                <?php the_title( sprintf ( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-                <?php the_content(); ?>
-                
-            <?php endwhile; ?> 
-            
-        <?php endif; ?>  
-        </div>
-        
-        <div class="col-md-4">
-        <?php get_sidebar(); ?>
-        </div>
-    </div>
-</div>
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
 
-<?php get_footer(); ?>
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
